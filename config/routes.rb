3a194ccad5ccff9667da71ec6ece72d2127cda09
vitalyliber <zenamax@gmail.com>
2017-11-17 14:48:44 +0500

    added a gem device

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # root to: "home#index"
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   post "/graphql", to: "graphql#execute"
 end
